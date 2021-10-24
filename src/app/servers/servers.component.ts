a17ec3d473b99e5cc4de36e9ca60fa3af19c2006
Lev Pasichnyi <lev.pasichnyi@gmail.com>
2021-10-24 19:57:48 +0300

    add use of selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers], - able to use as attribute
+  // selector: '.app-servers', - or as a class
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.less']
 })
