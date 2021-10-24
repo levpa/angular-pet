842cf96595d18881c017a79fec674e2b115695c4
Lev Pasichnyi <lev.pasichnyi@gmail.com>
2021-10-24 19:51:42 +0300

    use nested components in the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  //templateUrl: './app.component.html',
+  template: `
+    <h3> My main component </h3>
+    <app-servers></app-servers>
+    <app-servers></app-servers>`,
+  // styleUrls: ['./app.component.less']
+  styles: [`
+    h3 {
+      color: blue;
+    }
+  `]
 })
 export class AppComponent {
   title = 'angular-pet';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
